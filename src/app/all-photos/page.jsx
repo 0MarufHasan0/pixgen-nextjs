01d8cfe49a9c141f081862cb20a7b0b2da3de82b
0MarufHasan0 <marufhasan11.11.2323@gmail.com>
2026-04-30 12:44:12 +0600

    all done

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,15 +1,21 @@
+import Categories from '@/components/Categories';
 import PhotoCard from '@/components/PhotoCard';
 import React from 'react';
 
-const AllPhoto = async () => {
+const AllPhoto = async ({searchParams}) => {
+    const {category} = await searchParams
      const res = await fetch ('https://pixgen-nextjs.vercel.app/data.json')
     const photos = await res.json()
+
+    const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase() == category.toLowerCase()) :photos
     return (
         <div>
             <h1 className='text-2xl font-bold m-4'>All Photos</h1>
 
+            <Categories/>
+
             <div className='grid grid-cols-4 gap-4'>
-                {photos.map((photo) => (
+                {filteredPhotos.map((photo) => (
                     <PhotoCard key={photo.id} photo={photo} />
                 ))}
             </div>
